ac351782e84c197be314f934b9e2071474573a47
cyberbuddy-manas <rspmanas1@gmail.com>
2023-08-26 15:02:55 +0530

    adding changes

--- gdsc/main.js
+++ gdsc/main.js
@@ -1,11 +1,33 @@
-function func() {
+function capitalizeWords(str) {
+   // Split the string into an array of words
+   const words = str.split(' ');
+
+   // Capitalize the first letter of each word and join them back together
+   const capitalizedWords = words.map(word => {
+      // Ensure the word is not an empty string
+      if (word.length > 0) {
+         return word[0].toUpperCase() + word.slice(1);
+      } else {
+         return '';
+      }
+   });
+
+   // Join the capitalized words with a space to form the final string
+   const result = capitalizedWords.join(' ');
+
+   return result;
+}
+
+function func1() {
    let url = document.getElementById("name").value;
    if (!url) {
       alert("Please enter your name");
       return;
    }
 
+   url = capitalizeWords(url);
+
    document.getElementById("btn").href = "./Certificates/" + url + ".pdf";
-   document.getElementById("btn").download = url + ".pdf";
+   document.getElementById("btn").download = "./Certificates/" + url + ".pdf";
    document.getElementById("btn").click();
 }
