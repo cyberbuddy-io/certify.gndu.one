64bcf07569892767ba0181069e7750fee6b26797
cyberbuddy-manas <rspmanas1@gmail.com>
2023-09-10 00:09:49 +0530

    adding changes

--- algozenith/cs-orientation/main.js
+++ algozenith/cs-orientation/main.js
@@ -1,38 +1,87 @@
-function capitalizeWords(str) {
-   // Split the string into an array of words
-   const words = str.split(' ');
-
-   // Capitalize the first letter of each word and join them back together
-   const capitalizedWords = words.map(word => {
-      // Ensure the word is not an empty string
-      if (word.length > 0) {
-         return word[0].toUpperCase() + word.slice(1);
-      } else {
-         return '';
-      }
-   });
-
-   // Join the capitalized words with a space to form the final string
-   const result = capitalizedWords.join(' ');
-
-   return result;
+function alertBoot(text) {
+   document.querySelector(".alert h6").innerHTML = text;
+   $(".alert").show("fade");
+   setTimeout(() => {
+      $('.alert').hide('fade');
+   }, 4000);
 }
 
-function func1() {
+function viewCerti() {
    let url = document.querySelector(".sBtn-text").innerHTML;
-   if (!url) {
-      alert("Please enter your name");
+   if (url == "Select your name") {
+      alertBoot("Please select your name");
       return;
    }
-
-   url = capitalizeWords(url);
-
-   document.getElementById("btn").href = "./Certificates/" + url + ".pdf";
-   document.getElementById("btn").download = "./Certificates/" + url + ".pdf";
-   document.getElementById("btn").click();
+   document.getElementById("btn1").href = "./Certificates/" + url + ".pdf";
+   document.getElementById("btn1").click();
 }
 
-let names = ["aditya dadwal", "saksham", "bhavishya", "sayam"];
+function downloadCerti() {
+   let url = document.querySelector(".sBtn-text").innerHTML;
+   if (url == "Select your name") {
+      alertBoot("Please select your name");
+      return;
+   }
+   document.getElementById("btn2").download = "./Certificates/" + url + ".pdf";
+   document.getElementById("btn2").click();
+}
+
+function copyCerti() {
+   let url = document.querySelector(".sBtn-text").innerHTML;
+   if (url == "Select your name") {
+      alertBoot("Please select your name");
+      return;
+   }
+   navigator.clipboard.writeText(location.href + "./Certificates/" + url + ".pdf");
+   alertBoot("Link Copied to clipboard");
+}
+
+let names = [
+   "Jaideep Singh",
+   "Sahil Chabra",
+   "Lavuluri Adarsh Royal",
+   "Vanshi Puri",
+   "Vishal Yadav",
+   "Tanish Vansil",
+   "Shaurya khosla",
+   "Jasleen Kaur",
+   "Aakriti Mathur",
+   "Akshita raina",
+   "Manoj Lakhera",
+   "Sneha Goswami",
+   "Naman Dhingra",
+   "Kartik Sharma",
+   "Vimanpreet kaur",
+   "Kanan mahajan",
+   "Jashan khokhar",
+   "Md Yunus",
+   "Kirandeep Kaur",
+   "parneet kaur",
+   "Sarthak Mittal",
+   "Sahibjot Singh",
+   "Kunal Khanna",
+   "Vishnu Pratap Singh",
+   "Sachin singh",
+   "Rohain Singh Aulakh",
+   "Samarjeet Singh",
+   "Sourav Pathania",
+   "Ashwath Soni",
+   "Manseerat Kaur",
+   "Harshleen Kaur",
+   "Yuvika dabur",
+   "Priyansh",
+   "Jatin kumar",
+   "Ganak Aggarwal",
+   "Mahima Mahajan",
+   "Parth Bhagat",
+   "Aditya dadwal",
+   "Harshwardhan Singh",
+   "Harmanmeet Kaur",
+   "Roushni Sharma",
+   "Vasu Jindal"
+]
+
+names.sort();
 
 function createDrop(name) {
    const li = document.createElement("li");
@@ -55,7 +104,7 @@ function createDrop(name) {
    document.getElementsByClassName("options")[0].append(li);
 }
 
-for(let i in names) {
+for (let i in names) {
    createDrop(names[i]);
 }
 
@@ -63,11 +112,11 @@ const optionMenu = document.querySelector(".select-menu"),
    selectBtn = optionMenu.querySelector(".select-btn"),
    options = optionMenu.querySelectorAll(".option"),
    sBtn_text = optionMenu.querySelector(".sBtn-text");
-selectBtn.addEventListener("click", () => optionMenu.classList.toggle("active"));
-options.forEach(option => {
-   option.addEventListener("click", () => {
-      let selectedOption = option.querySelector(".option-text").innerText;
-      sBtn_text.innerText = selectedOption;
-      optionMenu.classList.remove("active");
+   selectBtn.addEventListener("click", () => optionMenu.classList.toggle("active"));
+   options.forEach(option => {
+      option.addEventListener("click", () => {
+         let selectedOption = option.querySelector(".option-text").innerText;
+         sBtn_text.innerText = selectedOption;
+         optionMenu.classList.remove("active");
+      });
    });
-});
